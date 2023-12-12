3f8cef328497ade682b2aaca9aaa55e8c9be4281
Kuba Bigaj <k.bigaj666@gmail.com>
2023-12-12 23:41:02 +0100

    Dodano resetowanie automatycznego id w czyszeczeniu danych

--- BD2_clear_data.sql
+++ BD2_clear_data.sql
@@ -1,28 +1,55 @@
--- Skrypt czyszczący dane ze wszystkich tabel bazy danych
+-- Skrypt czyszczący dane ze wszystkich tabel bazy danych i resetujący automatyczne id nadawane wpisom
 
--- Wyczyść wszystkie dane z tabeli `kategorie`
+-- Wyczyść dane z tabeli `kategorie`
 DELETE FROM kategorie;
 
--- Wyczyść wszystkie dane z tabeli `producenci`
+-- Zresetuj sekwencję generującą ID dla tabeli `kategorie`
+ALTER SEQUENCE seq_kategorie_id RESTART;
+
+-- Wyczyść dane z tabeli `producenci`
 DELETE FROM producenci;
 
--- Wyczyść wszystkie dane z tabeli `rozmiary`
+-- Zresetuj sekwencję generującą ID dla tabeli `producenci`
+ALTER SEQUENCE seq_producenci_id RESTART;
+
+-- Wyczyść dane z tabeli `rozmiary`
 DELETE FROM rozmiary;
 
--- Wyczyść wszystkie dane z tabeli `adresy`
+-- Zresetuj sekwencję generującą ID dla tabeli `rozmiary`
+ALTER SEQUENCE seq_rozmiary_id RESTART;
+
+-- Wyczyść dane z tabeli `adresy`
 DELETE FROM adresy;
 
--- Wyczyść wszystkie dane z tabeli `uzytkownicy`
+-- Zresetuj sekwencję generującą ID dla tabeli `adresy`
+ALTER SEQUENCE seq_adresy_id RESTART;
+
+-- Wyczyść dane z tabeli `uzytkownicy`
 DELETE FROM uzytkownicy;
 
--- Wyczyść wszystkie dane z tabeli `klienci`
+-- Zresetuj sekwencję generującą ID dla tabeli `uzytkownicy`
+ALTER SEQUENCE seq_uzytkownicy_id RESTART;
+
+-- Wyczyść dane z tabeli `klienci`
 DELETE FROM klienci;
 
--- Wyczyść wszystkie dane z tabeli `zamowienia`
+-- Zresetuj sekwencję generującą ID dla tabeli `klienci`
+ALTER SEQUENCE seq_klienci_id RESTART;
+
+-- Wyczyść dane z tabeli `zamowienia`
 DELETE FROM zamowienia;
 
--- Wyczyść wszystkie dane z tabeli `zamowienia_produkty`
+-- Zresetuj sekwencję generującą ID dla tabeli `zamowienia`
+ALTER SEQUENCE seq_zamowienia_id RESTART;
+
+-- Wyczyść dane z tabeli `zamowienia_produkty`
 DELETE FROM zamowienia_produkty;
 
--- Wyczyść wszystkie dane z tabeli `zwroty`
+-- Zresetuj sekwencję generującą ID dla tabeli `zamowienia_produkty`
+ALTER SEQUENCE seq_zamowienia_produkty_id RESTART;
+
+-- Wyczyść dane z tabeli `zwroty`
 DELETE FROM zwroty;
+
+-- Zresetuj sekwencję generującą ID dla tabeli `zwroty`
+ALTER SEQUENCE seq_zwroty_id RESTART;
