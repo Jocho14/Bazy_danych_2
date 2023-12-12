04bb95b3a29d1fddae10e8103949880f953b02ab
Kuba Bigaj <k.bigaj666@gmail.com>
2023-12-12 23:32:13 +0100

    Dodano skrypt czyszczący BD.

---
+++ BD2_clear_data.sql
@@ -0,0 +1,28 @@
+-- Skrypt czyszczący dane ze wszystkich tabel bazy danych
+
+-- Wyczyść wszystkie dane z tabeli `kategorie`
+DELETE FROM kategorie;
+
+-- Wyczyść wszystkie dane z tabeli `producenci`
+DELETE FROM producenci;
+
+-- Wyczyść wszystkie dane z tabeli `rozmiary`
+DELETE FROM rozmiary;
+
+-- Wyczyść wszystkie dane z tabeli `adresy`
+DELETE FROM adresy;
+
+-- Wyczyść wszystkie dane z tabeli `uzytkownicy`
+DELETE FROM uzytkownicy;
+
+-- Wyczyść wszystkie dane z tabeli `klienci`
+DELETE FROM klienci;
+
+-- Wyczyść wszystkie dane z tabeli `zamowienia`
+DELETE FROM zamowienia;
+
+-- Wyczyść wszystkie dane z tabeli `zamowienia_produkty`
+DELETE FROM zamowienia_produkty;
+
+-- Wyczyść wszystkie dane z tabeli `zwroty`
+DELETE FROM zwroty;
