b17db3d1e761267c0406aaeb6aaa6367b79f4887
Kuba Bigaj <k.bigaj666@gmail.com>
2023-12-18 13:05:05 +0100

    Dodano triggery i indeksy

--- BD2_triggery.sql
+++ BD2_triggery.sql
@@ -1,14 +1,16 @@
-CREATE TRIGGER zwolnienie_pracownika
+CREATE TRIGGER tri_zwolnienie_pracownika
 AFTER UPDATE
 ON pracownicy
 FOR EACH ROW
 BEGIN
 	IF NEW.data_zwolnienia IS NOT NULL THEN
-		SET NEW.konto_aktywne = FALSE;
+		UPDATE pracownicy
+		SET NEW.konto_aktywne = FALSE
+		WHERE id_pracownik = NEW.id_pracownik;
     END IF;
 END;
 
-CREATE TRIGGER kupno_produktow
+CREATE TRIGGER tri_kupno_produktow
 AFTER INSERT
 ON zamowienia_produkty
 FOR EACH ROW
@@ -18,7 +20,7 @@ BEGIN
 	WHERE produkty.id_produktu = NEW.id_produktu;
 END;
 
-CREATE TRIGGER cofniecie_kupna_produktow
+CREATE TRIGGER tri_cofniecie_kupna_produktow
 AFTER DELETE
 ON zamowienia_produkty
 FOR EACH ROW
@@ -27,3 +29,23 @@ BEGIN
 	SET produkty.ilosc_w_magazynie = produkty.ilosc_w_magazynie + OLD.ilosc
 	WHERE produkty.id_produktu = OLD.id_produktu;
 END;
+
+CREATE OR REPLACE FUNCTION wylicz_cene_zamowienia() RETURNS TRIGGER AS $$
+DECLARE
+  total_amount DECIMAL(10, 2);
+BEGIN
+  SELECT SUM(cena_produktu * ilosc) INTO total_amount
+  FROM zamowienia_produkty
+  WHERE id_zamowienia = NEW.id_zamowienia;
+
+  UPDATE zamowienia
+  SET cena_zakupu = total_amount
+  WHERE id_zamowienia = NEW.id_zamowienia;
+
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER tri_cena_zamowienia
+AFTER INSERT OR UPDATE ON zamowienia_produkty
+FOR EACH ROW EXECUTE FUNCTION wylicz_cene_zamowienia();
