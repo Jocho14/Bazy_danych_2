710dcd80c1086c55caed6a34d5cc8d94e5e02ac3
Kuba Bigaj <k.bigaj666@gmail.com>
2023-12-21 19:41:49 +0100

    a

--- BD2_users.sql
+++ BD2_users.sql
@@ -4,7 +4,6 @@ CREATE USER client LOGIN PASSWORD 'access';
 
 CREATE USER admin SUPERUSER LOGIN PASSWORD 'admin';
 
-<<<<<<< HEAD
 GRANT
 SELECT
   ON TABLE produkty,
@@ -16,12 +15,6 @@ SELECT
 GRANT
 SELECT
   ON ALL TABLES IN SCHEMA "public" TO client;
-=======
-
-GRANT SELECT ON TABLE produkty, rozmiary, producenci, kategorie, detale_produktow TO guest;
-
-GRANT SELECT ON ALL TABLES IN SCHEMA "public" TO client;
->>>>>>> 7e23f40c08b40b0a37309e5cb340a2a4939b045d
 
 GRANT EXECUTE ON FUNCTION dodaj_uzytkownika(
   arg_login VARCHAR(64),
@@ -35,7 +28,6 @@ GRANT EXECUTE ON FUNCTION dodaj_uzytkownika(
   arg_adres_nr_domu VARCHAR(10),
   arg_adres_nr_lokalu VARCHAR(10),
   arg_adres_kod_pocztowy VARCHAR(6)
-<<<<<<< HEAD
 ),
 logowanie(
   arg_login VARCHAR(64),
@@ -52,10 +44,3 @@ GRANT
 INSERT
   ON TABLE zamowienia,
   zamowienia_produkty TO client;
-=======
-) TO client, guest;
-
-GRANT INSERT ON TABLE klienci TO guest, client;
-
-GRANT INSERT ON TABLE zamowienia, zamowienia_produkty TO client;
->>>>>>> 7e23f40c08b40b0a37309e5cb340a2a4939b045d
