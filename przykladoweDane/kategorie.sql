d63ef3de128599c4f42cbd8d539312c865914a63
Jocho14 <106482177+Jocho14@users.noreply.github.com>
2023-12-22 12:23:55 +0100

    przykładowe dane

---
+++ przykladoweDane/kategorie.sql
@@ -0,0 +1,4 @@
+Insert Into kategorie (id_kategorii, kategoria) Values (1, 'bielizna');
+Insert Into kategorie (id_kategorii, kategoria) Values (2, 'bluzki');
+Insert Into kategorie (id_kategorii, kategoria) Values (3, 'spodnie');
+Insert Into kategorie (id_kategorii, kategoria) Values (4, 'nakrycia głowy');
---
+++ przykladoweDane/producenci.sql
@@ -0,0 +1,3 @@
+Insert Into producenci (id_producenta, producent) Values (1, 'Cotton World');
+Insert Into producenci (id_producenta, producent) Values (2, 'Cropp');
+Insert Into producenci (id_producenta, producent) Values (3, 'Helikon-Tex');
