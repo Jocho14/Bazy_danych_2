985195067f5a6d994da002a474086d0f0bf38339
Kuba Bigaj <105283349+Kuba-Bigaj@users.noreply.github.com>
2024-01-07 17:46:36 +0100

    Update producenci.sql

--- przykladoweDane/producenci.sql
+++ przykladoweDane/producenci.sql
@@ -1,3 +1,10 @@
-Insert Into producenci (id_producenta, producent) Values (1, 'Cotton World');
-Insert Into producenci (id_producenta, producent) Values (2, 'Cropp');
-Insert Into producenci (id_producenta, producent) Values (3, 'Helikon-Tex');
+Insert Into producenci (id_producenta, producent) Values (1, 'H&M');
+Insert Into producenci (id_producenta, producent) Values (2, 'Nike');
+Insert Into producenci (id_producenta, producent) Values (3, 'Adidas');
+Insert Into producenci (id_producenta, producent) Values (4, 'Gap');
+Insert Into producenci (id_producenta, producent) Values (5, 'Lululemon Athletica');
+Insert Into producenci (id_producenta, producent) Values (6, 'American Eagle Outfitters');
+Insert Into producenci (id_producenta, producent) Values (7, 'Puma SE');
+Insert Into producenci (id_producenta, producent) Values (8, 'Under Armour');
+Insert Into producenci (id_producenta, producent) Values (9, 'Fast Retailing');
+Insert Into producenci (id_producenta, producent) Values (10, 'Inditex');
