7fb0dc90ec212c2a3cbe5509dc882d281ab5f3b7
Kuba Bigaj <105283349+Kuba-Bigaj@users.noreply.github.com>
2024-01-07 17:42:11 +0100

    Update kategorie.sql

--- przykladoweDane/kategorie.sql
+++ przykladoweDane/kategorie.sql
@@ -1,4 +1,9 @@
-Insert Into kategorie (id_kategorii, kategoria) Values (1, 'bielizna');
-Insert Into kategorie (id_kategorii, kategoria) Values (2, 'bluzki');
-Insert Into kategorie (id_kategorii, kategoria) Values (3, 'spodnie');
-Insert Into kategorie (id_kategorii, kategoria) Values (4, 'nakrycia głowy');
+Insert Into kategorie (id_kategorii, kategoria) Values (1, 'buty');
+Insert Into kategorie (id_kategorii, kategoria) Values (2, 'bluzy i swetry');
+Insert Into kategorie (id_kategorii, kategoria) Values (3, 'dodatki');
+Insert Into kategorie (id_kategorii, kategoria) Values (4, 'koszule');
+Insert Into kategorie (id_kategorii, kategoria) Values (5, 'kurtki');
+Insert Into kategorie (id_kategorii, kategoria) Values (6, 'nakrycia glowy');
+Insert Into kategorie (id_kategorii, kategoria) Values (7, 'rekawiczki');
+Insert Into kategorie (id_kategorii, kategoria) Values (8, 'spodnie i jeansy');
+Insert Into kategorie (id_kategorii, kategoria) Values (9, 't-shirty');
