1d61a604c7e18eb11aeb52eb8bdf7484e55dc759
Jocho14 <106482177+Jocho14@users.noreply.github.com>
2024-01-11 14:04:42 +0100

    poprawka

--- przykladoweDane/producenci.sql
+++ przykladoweDane/producenci.sql
@@ -3,7 +3,7 @@ Insert Into producenci (id_producenta, producent) Values (2, 'Nike');
 Insert Into producenci (id_producenta, producent) Values (3, 'Adidas');
 Insert Into producenci (id_producenta, producent) Values (4, 'Gap');
 Insert Into producenci (id_producenta, producent) Values (5, 'Lululemon Athletica');
-Insert Into producenci (id_producenta, producent) Values (6, 'American Eagle Outfitters');
+Insert Into producenci (id_producenta, producent) Values (6, 'ZARA');
 Insert Into producenci (id_producenta, producent) Values (7, 'Puma SE');
 Insert Into producenci (id_producenta, producent) Values (8, 'Under Armour');
 Insert Into producenci (id_producenta, producent) Values (9, 'Fast Retailing');
